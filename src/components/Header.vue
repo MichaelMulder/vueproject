6ac00c68d99d4de747a456c29dfe7477af332378
MichaelMulder <micahelorenmulder@gmail.com>
2019-02-25 23:59:38 -0600

    Added icons and made base the base for header component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-toolbar app dark>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>Buy & Sell</span>
+      <span class="font-weight-light">\Stuff</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-badge>
+      <span slot="badge">{{cart}}</span>
+      <v-icon @click="viewCart">mdi-cart</v-icon>
+    </v-badge>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      // TODO
+    };
+  }
+};
+</script>
